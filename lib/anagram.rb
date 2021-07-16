8a16b7a6ac3e24033f45225b6e8b6da3ba34e418
Andrew Giang <giang184@gmail.com>
2021-07-16 08:55:42 -0700

    pass test for two scrambled words

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,5 @@
 def anagram(word1, word2)
-  if word1 == word2
+  if word1.chars.sort.join == word2.chars.sort.join
     "'#{word1}' and '#{word2}' ARE anagrams"
   else
     "'#{word1}' and '#{word2}' ARE NOT anagrams"
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,10 +3,13 @@ require('anagram')
 require('pry')
 
 describe('#title_case') do
-  it("Checks if two words are anagrams") do
+  it("Checks if two identical words are anagrams") do
     expect(anagram('hello', 'hello')).to(eq("'hello' and 'hello' ARE anagrams"))
   end
-  it("Checks if two words are not anagrams") do
+  it("Checks that two different words are not anagrams") do
     expect(anagram('hello', 'oell')).to(eq("'hello' and 'oell' ARE NOT anagrams"))
+  end
+  it("Checks if two scrambled words are anagrams") do
+    expect(anagram('hello', 'olleh')).to(eq("'hello' and 'olleh' ARE anagrams"))
   end
 end
