3d26d210cda4acb9dd2486dbbb33389182d31801
Andrew Giang <giang184@gmail.com>
2021-07-16 08:23:26 -0700

    initiate files

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -6,7 +6,6 @@ describe('#title_case') do
   it("capitalizes the first letter of a word") do
     expect(title_case("beowulf")).to(eq("Beowulf"))
   end
-
   it("capitalizes the first letter of all words in a multiple word title") do
     expect(title_case("the color purple")).to(eq("The Color Purple"))
   end
