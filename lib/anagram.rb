ba50139c0396eb91b5d6b132d96bab5165e8b0ab
Andrew Giang <giang184@gmail.com>
2021-07-16 10:31:11 -0700

    refactor to use class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,24 @@
-def anagram(word1, word2)
-  temp1 = word1.downcase.chars.sort.join.gsub(/[^a-z]/, '')
-  temp2 = word2.downcase.chars.sort.join.gsub(/[^a-z]/, '')
-  if !/[aeiouy]/.match?(temp1)
-    "'#{word1}' is not a word"
-  elsif (!/[aeiouy]/.match?(temp2))
-    "'#{word2}' is not a word"
-  elsif temp1 == temp2
-    "'#{word1}' and '#{word2}' are ANAGRAMS"
-  elsif !(/[#{temp1}]/.match?(temp2))
-    "'#{word1}' and '#{word2}' are ANTIGRAMS"
-  else
-    "'#{word1}' and '#{word2}' ARE NEITHER anagrams nor antigrams"
+class Words
+  attr_reader(:word1, :word2)
+
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+
+  def anagram()
+    temp1 = @word1.downcase.chars.sort.join.gsub(/[^a-z]/, '')
+    temp2 = @word2.downcase.chars.sort.join.gsub(/[^a-z]/, '')
+    if !/[aeiouy]/.match?(temp1)
+      "'#{@word1}' is not a word"
+    elsif (!/[aeiouy]/.match?(temp2))
+      "'#{@word2}' is not a word"
+    elsif temp1 == temp2
+      "'#{@word1}' and '#{@word2}' are ANAGRAMS"
+    elsif !(/[#{temp1}]/.match?(temp2))
+      "'#{@word1}' and '#{@word2}' are ANTIGRAMS"
+    else
+      "'#{@word1}' and '#{@word2}' ARE NEITHER anagrams nor antigrams"
+    end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,27 +4,35 @@ require('pry')
 
 describe('#anagram') do
   it("Checks if two identical words are anagrams") do
-    expect(anagram('hello', 'hello')).to(eq("'hello' and 'hello' are ANAGRAMS"))
+    my_words = Words.new('hello', 'hello')
+    expect(my_words.anagram()).to(eq("'hello' and 'hello' are ANAGRAMS"))
   end
   it("Checks that two different words are not anagrams") do
-    expect(anagram('hello', 'oell')).to(eq("'hello' and 'oell' ARE NEITHER anagrams nor antigrams"))
+    my_words = Words.new('hello', 'oell')
+    expect(my_words.anagram()).to(eq("'hello' and 'oell' ARE NEITHER anagrams nor antigrams"))
   end
   it("Checks if two scrambled words are anagrams") do
-    expect(anagram('hello', 'olleh')).to(eq("'hello' and 'olleh' are ANAGRAMS"))
+    my_words = Words.new('hello', 'olleh')
+    expect(my_words.anagram()).to(eq("'hello' and 'olleh' are ANAGRAMS"))
   end
   it("Checks if two scrambled words are anagrams, accouting for the possibility for lower and upper case") do
-    expect(anagram('Tea', 'Eat')).to(eq("'Tea' and 'Eat' are ANAGRAMS"))
+    my_words = Words.new('Tea', 'Eat')
+    expect(my_words.anagram()).to(eq("'Tea' and 'Eat' are ANAGRAMS"))
   end
   it("Checks if word1 is a word w/o vowels") do
-    expect(anagram('stmp', 'Eat')).to(eq("'stmp' is not a word"))
+    my_words = Words.new('stmp', 'Eat')
+    expect(my_words.anagram()).to(eq("'stmp' is not a word"))
   end
   it("Checks if word2 is a word w/o vowels") do
-    expect(anagram('foo', 'jklt')).to(eq("'jklt' is not a word"))
+    my_words = Words.new('foo', 'jklt')
+    expect(my_words.anagram()).to(eq("'jklt' is not a word"))
   end
   it("Checks for antigrams") do
-    expect(anagram('foo', 'kit')).to(eq("'foo' and 'kit' are ANTIGRAMS"))
+    my_words = Words.new('foo', 'kit')
+    expect(my_words.anagram()).to(eq("'foo' and 'kit' are ANTIGRAMS"))
   end
   it("Checks if two sentences are anagrams") do
-    expect(anagram('The Morse Code', 'Here come dots!')).to(eq("'The Morse Code' and 'Here come dots!' are ANAGRAMS"))
+    my_words = Words.new('The Morse Code', 'Here come dots!')
+    expect(my_words.anagram()).to(eq("'The Morse Code' and 'Here come dots!' are ANAGRAMS"))
   end
 end
